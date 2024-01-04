d5fe47f1c2c8139007a9d0d7de19f427cb5a6240
MarcusL00 <118048523+MarcusL00@users.noreply.github.com>
2024-01-04 13:56:53 +0100

    more updates to site

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ menu.addEventListener('click', function () {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+  const buttons = document.querySelectorAll('.toggle-button');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', function () {
+      buttons.forEach(btn => btn.classList.remove('active'));
+      this.classList.add('active');
+    });
+  });
+});
