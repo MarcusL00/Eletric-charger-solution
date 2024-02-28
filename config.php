ea5f9721d035d2774a0a2dc52eff42ddc567cdf6
Marcus Wind Lystrup (MARC128P) <marc128p@zbc.dk>
2024-02-28 09:43:11 +0100

    cringe syntax error
    cringe mistakes were fixed

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
 //  only allowes for the website to handout session id.
-ini_set('session.use_only_cookies',1)
-ini_set('session.use_stríct_mode',1)
+ini_set('session.use_only_cookies',1);
+ini_set('session.use_stríct_mode',1);
 
-session_set_cookie_param([
+session_set_cookie_params([
     'lifetime' => 1800,
     'domain' => 'localhost',
     'path' => '/',
